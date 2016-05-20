5d3ab88a3960a3e5ecde956d135b330303547070
4yn <changsiyuan1999@gmail.com>
2016-05-20 01:44:00 +0100

    start on gp whee

---
+++ gp/makefiles.sh
@@ -0,0 +1,45 @@
+
+mkdir ./content/01_Media
+cp template.tex ./content/01_Media/01_Media.tex
+cp template.tex ./content/01_Media/01_A_Privacy.tex
+cp template.tex ./content/01_Media/01_B_Advertising.tex
+cp template.tex ./content/01_Media/01_C_New_Media.tex
+
+mkdir ./content/02_Social_Issues
+cp template.tex ./content/02_Social_Issues/02_Social_Issues.tex
+cp template.tex ./content/02_Social_Issues/02_A_Equity_and_Inequality.tex
+cp template.tex ./content/02_Social_Issues/02_B_Money_and_Poverty.tex
+cp template.tex ./content/02_Social_Issues/02_C_Ageing.tex
+cp template.tex ./content/02_Social_Issues/02_D_Gender.tex
+cp template.tex ./content/02_Social_Issues/02_E_Family.tex
+cp template.tex ./content/02_Social_Issues/02_F_Crime_and_Punishment.tex
+cp template.tex ./content/02_Social_Issues/02_G_Identity.tex
+cp template.tex ./content/02_Social_Issues/02_H_Rights.tex
+
+mkdir ./content/03_Arts
+cp template.tex ./content/03_Arts/03_Arts.tex
+cp template.tex ./content/03_Arts/03_A_Culture.tex
+
+mkdir ./content/04_Science
+cp template.tex ./content/04_Science/04_Science.tex
+cp template.tex ./content/04_Science/04_A_Environment.tex
+cp template.tex ./content/04_Science/04_B_Education.tex
+cp template.tex ./content/04_Science/04_C_Medicine_and_Ethics.tex
+cp template.tex ./content/04_Science/04_D_New_Technology.tex
+
+mkdir ./content/05_Politics
+cp template.tex ./content/05_Politics/05_Politics.tex
+cp template.tex ./content/05_Politics/05_A_Democracy.tex
+cp template.tex ./content/05_Politics/05_B_Global_Government.tex
+
+mkdir ./content/06_Philosophy
+cp template.tex ./content/06_Philosophy/06_Philosophy.tex
+cp template.tex ./content/06_Philosophy/06_A_The_Human_Condition.tex
+
+mkdir ./content/07_Essay_Technique
+cp template.tex ./content/07_Essay_Technique/07_Essay_Technique.tex
+cp template.tex ./content/07_Essay_Technique/07_A_Question_Analysis.tex
+cp template.tex ./content/07_Essay_Technique/07_B_Overall_Strucure.tex
+cp template.tex ./content/07_Essay_Technique/07_C_Intro.tex
+cp template.tex ./content/07_Essay_Technique/07_D_Body_Paragraphs.tex
+cp template.tex ./content/07_Essay_Technique/07_E_Conclusion.tex
